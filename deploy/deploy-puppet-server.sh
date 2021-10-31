406601f7b58a1207d34c3b91c0ec702128e593bb
Graham Smith <gsmith@gmsnet.co.uk>
2021-10-31 09:31:27 +0000

    update

--- deploy/deploy-puppet-server.sh
+++ deploy/deploy-puppet-server.sh
@@ -2,6 +2,11 @@
 HOSTNAME=$(/bin/hostname)
 hostnamectl set-hostname ${HOSTNAME}.gmslab.local
 
-yum install -y http://yum.puppetlabs.com/puppet7/puppet7-release-el-7.noarch.rpm
-yum install -y puppetserver
+yum update -y
+
+yum install git -y
+git config --global credential.helper store
+
+#yum install -y http://yum.puppetlabs.com/puppet7/puppet7-release-el-7.noarch.rpm
+#yum install -y puppetserver
 
