5e5845679df024dfc7167f091da08f67d4e2e04a
Graham Smith <gsmith@gmsnet.co.uk>
2021-12-29 19:47:52 +0000

    debug

--- deploy/deploy-puppet-server.sh
+++ deploy/deploy-puppet-server.sh
@@ -18,25 +18,26 @@ KEYS_DIR="/root/keys"
 # Get Puppet EYAML Private Key
 URI='https://keyvault-gms.vault.azure.net/secrets/puppet-eyaml-private?api-version=2016-10-01'
 OUTPUT="${KEYS_DIR}/private_key.pkcs7.pem"
-/bin/curl -s $URI -H $HEADER | /bin/python -c "import sys, json; print json.load(sys.stdin)['access_token']" > $OUTPUT
+/bin/curl -s $URI -H "$HEADER" | /bin/python -c "import sys, json; print json.load(sys.stdin)['value']" > $OUTPUT
 
 # Get Puppet EYAML Public Key
 URI='https://keyvault-gms.vault.azure.net/secrets/puppet-eyaml-public?api-version=2016-10-01'
 OUTPUT="${KEYS_DIR}/public_key.pkcs7.pem"
-/bin/curl -s $URI -H $HEADER | /bin/python -c "import sys, json; print json.load(sys.stdin)['access_token']" > $OUTPUT
+/bin/curl -s $URI -H "$HEADER" | /bin/python -c "import sys, json; print json.load(sys.stdin)['value']" > $OUTPUT
 
 # Get Github Access Private Key
 URI='https://keyvault-gms.vault.azure.net/secrets/id-github-private?api-version=2016-10-01'
 OUTPUT="${KEYS_DIR}/id_github"
-/bin/curl -s $URI -H $HEADER | /bin/python -c "import sys, json; print json.load(sys.stdin)['access_token']" > $OUTPUT
+/bin/curl -s $URI -H "$HEADER" | /bin/python -c "import sys, json; print json.load(sys.stdin)['value']" > $OUTPUT
 
 # Get Github Access Public Key
 URI='https://keyvault-gms.vault.azure.net/secrets/id-github-public?api-version=2016-10-01'
 OUTPUT="${KEYS_DIR}/id_github.pub"
-/bin/curl -s $URI -H $HEADER | /bin/python -c "import sys, json; print json.load(sys.stdin)['access_token']" > $OUTPUT
+/bin/curl -s $URI -H "$HEADER" | /bin/python -c "import sys, json; print json.load(sys.stdin)['value']" > $OUTPUT
 
 # Set permissions on key files
 /bin/chmod -R 700 ${KEYS_DIR}
+
 #yum update -y
 
 #yum install git -y
